f88747136f2b7bfb9ec2d9134c90d4062d4e436b
catto++ <84719557+CattoDoesCode@users.noreply.github.com>
2022-09-27 02:30:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,13 @@
 <html lang="en">
 
 <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $("#fname").keypress(function(e) {
-                if (e.keyCode >= 65 && e.keyCode <= 90) {
-                    e.preventDefault();
-                }
-            })
-        });
-    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Infix Calculator</title>
-
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <script src="jquery-3.6.1.min.js"></script>
+    <script src="index.js"></script>
     <style>
         body {
             background-color: rgb(29, 29, 29);
@@ -27,67 +18,62 @@
 </head>
 
 <body>
-    <?php include 'infix_calcu.php';
-
-    $input_val = "";
-    $ans_final = "";
-    ?>
+    <?php include 'infix_calcu.php'; ?>
 
     <div class="container mt-5">
-        <h1 class="display-4">Infix Calculator</h1>
-        <p>code by Jamora, Morales, Selerio | BSCS - C83</p>
+        <div class="row">
+            <h1 class="display-4">Infix Calculator</h1>
+            <p>code by Jamora, Morales, Selerio | BSCS - C83</p>
+        </div>
 
-        <div class="row p-3">
-            <div class="row mb-3 align-items-center" style="padding: 0;">
-                <label>Enter Infix Expression:</label><br>
-                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                    <input type="text" name="fname" class="fname" id="fname">
-                    <button type="submit" id="Calculate" class="btn btn-primary" style="width:auto;" onclick="">Calculate</button>
-                    <button type="button" id="clear-btn" class="btn btn-secondary" onclick="clear_values()" style="width:auto;">Clear</button>
-                </form>
-            </div>
-            <div class="row mt-3 mb-3">
-                <span class="lead" id="ans" style="padding: 0; font-size: 2rem;"> =
-                    <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $input = htmlspecialchars($_REQUEST['fname']); // collect value of input field
-                        $calcu->infix = $input;
-                        if ($calcu->error_trap() != 0) {
-                            if ($calcu->error_trap() == 1) {
-                                ##ALERT FOR ALPHABET
-                            }
-                            else if ($calcu->error_trap() == 2) {
-                                ##ALERT FOR INVALID SPECIAL CHARACTERS
-                            }
+        <div id="liveAlertPlaceholder"></div>
 
+        <div class="row mt-4 mb-4" style="padding: 0;">
+            <label>Enter Infix:</label><br>
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <input type="text" name="fname" id="input-field">
+                <button type="submit" id="Calculate" class="btn btn-primary" style="width:auto;" onclick="init()">Calculate</button>
+                <button type="button" id="clear-btn" class="btn btn-secondary" onclick="clear_values()" style="width:auto;">Clear</button>
+            </form>
+        </div>
+
+        <div class="row">
+            <span class="lead" id="ans" style="padding-left: 1ren;font-size: 2rem;"> =
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $input = htmlspecialchars($_REQUEST['fname']); // collect value of input field
+                    $calcu->infix = $input;
+
+                    if ($calcu->error_trap() != 0) {
+                        if ($calcu->error_trap() == 1) {
+                            echo '<script>alert("Invalid Input! letter detected")</script>';
+                        } else if ($calcu->error_trap() == 2) {
+                            echo '<script>alert("Invalid Input! invalid character detected")</script>';
                         }
-                        else {
-                            $calcu->infix_to_postfix();
-                            $calcu->stack_operation();
+                    } else {
+                        $calcu->infix_to_postfix();
+                        $calcu->stack_operation();
 
-                            if (empty($input)) {
-                                echo "0";
-                            } else {
-                                echo $calcu->final_ans;
-                            }
+                        if (empty($input)) {
+                            echo "0";
+                        } else {
+                            echo $calcu->final_ans;
                         }
                     }
-                    ?>
-                </span>
-            </div>
-            <div class="row mt-3 mb-3">
-                <div id="liveAlertPlaceholder"></div>   
-
-            </div>
+                }
+                ?>
+            </span>
         </div>
+
     </div>
 
-    <script>
-        function clear_values() {
-            document.getElementById("fname").value = "";
-            document.getElementById("ans").innerHTML = "= 0";
-        }
-    </script>
+    <div class="container mt-5" id="step-by-step">
+        <p style="font-style: italic;" id="infix">Infix:</p>
+        <?php echo $calcu->infix ?>
+        <br><br>
+        <p style="font-style: italic;" id="postfix">Postix:</p>
+        <?php $calcu->print_arr($calcu->postfix) ?>
+    </div>
 
 </body>
 
