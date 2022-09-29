db255467e1181c5debead49f9cd1ca3d2dbe1e5c
catto++ <84719557+CattoDoesCode@users.noreply.github.com>
2022-09-30 04:11:08 +0800

    addded index.js for bootstrap alerts

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
             background-color: rgb(29, 29, 29);
             color: whitesmoke;
         }
+        .accordion-item {
+            border: 1px solid whitesmoke;
+        }
+        .accordion-button:not(.collapsd) {
+            /* color: inherit; */
+            background: rgb(29, 29, 29);
+            color: whitesmoke;  
+        }
+        .accordion-button:not(.collapsed)::after {
+            filter: brightness(0%) invert(70%);
+        }
+        .accordion-button:focus {
+            box-shadow: inherit;
+        }
     </style>
 </head>
 
@@ -66,7 +80,7 @@
                     }
                 }
             } else { ?>
-                <div class="accordion accordion-flush bg-dark" id="accordionFlushExample">
+                <div class="accordion " id="accordionFlushExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingOne">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -98,31 +112,7 @@
         ?>
     </div>
 
-    <script>
-        function bootstrap_alert(title, message, type) {
-            const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-            const alert = (title, message, type) => {
-                const wrapper = document.createElement('div')
-                wrapper.innerHTML = [
-                    `<div class="alert alert-${type} alert-dismissible d-flex align-items-center fade show" role="alert">`,
-                    `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>`,
-                    `   <div><strong>${title}</strong>${message}</div>`,
-                    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-                    '</div>'
-                ].join('')
-
-                alertPlaceholder.append(wrapper)
-            }
-
-            alert(title, message, type)
-        }
-
-        function clear_values() {
-            document.getElementById("input-field").value = "";
-        }
-    </script>
+    <script src="index.js"></script>
     <script src="bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 
