7a88bf1a4096cc1da9545e3066c9272098cb0e5b
catto++ <84719557+CattoDoesCode@users.noreply.github.com>
2022-09-30 03:52:24 +0800

    removed unnecessary files

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Infix Calculator</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <script src="jquery-3.6.1.min.js"></script>
     <style>
         body {
             background-color: rgb(29, 29, 29);
@@ -99,7 +98,31 @@
         ?>
     </div>
 
-    <script src="index.js"></script>
+    <script>
+        function bootstrap_alert(title, message, type) {
+            const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+            const alert = (title, message, type) => {
+                const wrapper = document.createElement('div')
+                wrapper.innerHTML = [
+                    `<div class="alert alert-${type} alert-dismissible d-flex align-items-center fade show" role="alert">`,
+                    `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+  </svg>`,
+                    `   <div><strong>${title}</strong>${message}</div>`,
+                    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+                    '</div>'
+                ].join('')
+
+                alertPlaceholder.append(wrapper)
+            }
+
+            alert(title, message, type)
+        }
+
+        function clear_values() {
+            document.getElementById("input-field").value = "";
+        }
+    </script>
     <script src="bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 
